99a302072751177d74566cfa4034f1f1794346ea
alexandre.ribeiro <alexandre.ribeiro@mv.com.br>
2021-11-13 11:03:07 -0300

    craido o producer

---
+++ src/main/java/org/estudo/kafka/ecommerce/FraudDetectorService.java
@@ -0,0 +1,2 @@
+package org.estudo.kafka.ecommerce;public class FraudDetectorService {
+}
---
+++ src/main/java/org/estudo/kafka/ecommerce/NewOrderMain.java
@@ -0,0 +1,43 @@
+package org.estudo.kafka.ecommerce;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        //producer
+        var producer = new KafkaProducer<String, String>(properties());
+        var key= "chave";
+        var value= "10992310298,3232,44";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+        //funciona de forma assyncrono
+//        producer.send(record).get();
+        //criado um callBack para conseguir esperar o retorno
+        producer.send(record, (data, ex)->{
+            if (ex != null){
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("Sucesso enviado "+ data.topic()+ ":::partition "+ data.partition()+ "/ offset"+ data.offset()+ "/ timestamp "+data.timestamp());
+        }).get();
+
+
+
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        //key
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        //value
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
